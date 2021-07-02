6ee650121c7504abb9aba1245eefe638d73418bc
ElMurcielago98 <ElMurcielago98@users.noreply.github.com>
2021-07-02 23:55:31 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, var>
+    {
+        T[] keys;
+        var[] items;
+        public MyDictionary()
+        {
+            keys = new T[0];
+            items = new var[0];
+        }
+
+        public void Add(T key, var item)
+        {
+            T[] tempKeys = keys;
+            var[] tempItems = items;
+
+            keys = new T[keys.Length + 1];
+            items = new var[items.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                items[i] = tempItems[i];
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Berkay");
+        }
+    }
+}
